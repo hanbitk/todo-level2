f61b1f69786e6f52d7de3c5b5dc5ff1f98646639
Hanbit Kang <hanbitkang95@gmail.com>
2023-04-20 14:45:49 +0900

    App.jsx: Styled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+const StContainer = styled.div`
+  border: 1px solid red;
+  max-width: 1200px;
+  min-width: 800px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <StContainer>Hello</StContainer>;
 }
 
 export default App;
